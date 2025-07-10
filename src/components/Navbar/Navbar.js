ee087a979f4bf22bedcb67a6d07bf1343c2ec4f5
FacuuV <vazquezfacundo439@gmail.com>
2025-07-10 00:27:39 -0300

    navbar y estilos2responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  // 1. Creamos el estado para saber si el menú está abierto o no.
+  // Inicia en 'false' (cerrado).
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="container-navbar">
       <nav className="navbar container" id="inicio">
-        <ul className="menu">
+        {/* 2. Añadimos el icono de hamburguesa. Al hacerle clic,
+            cambiamos el estado de 'isMenuOpen' a su valor contrario. */}
+        <i className="fa-solid fa-bars" onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+
+        {/* 3. Usamos el estado para añadir la clase 'active' al menú si isMenuOpen es true */}
+        <ul className={`menu ${isMenuOpen ? 'active' : ''}`}>
           <li><a href="#inicio">Inicio</a></li>
           <li><a href="#peces">Peces</a></li>
           <li><a href="#accesorios">Accesorios</a></li>
@@ -25,4 +34,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
